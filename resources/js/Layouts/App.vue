3fa1c79fd51fda215480bb5c0d7be6ff10339fa0
Larry Barker <github@larrybarker.me>
2021-02-27 22:04:22 -0600

    refactor app layout

---
+++ resources/js/Layouts/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="flex flex-col min-h-screen overflow-hidden">
+
+        <Header />
+
+        <div class="min-h-screen bg-gray-900">
+
+            <!-- Page Content -->
+            <main class="flex-grow">
+                <slot />
+            </main>
+
+        </div>
+
+    </div>
+</template>
+
+<script>
+    import Header from '@/Components/Header'
+    import HeroIllustration from '@/Components/HeroIllustration'
+
+    export default {
+        components: {
+            Header,
+            HeroIllustration,
+        },
+    }
+</script>
